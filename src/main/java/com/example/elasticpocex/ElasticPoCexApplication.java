6f7573d7681d896f7c67079e399e54b1158489ce
vinus.agrawal <vaibhav.yadav@genesistechnologies.in>
2022-02-25 19:01:19 +0530

    first commit

---
+++ src/main/java/com/example/elasticpocex/ElasticPoCexApplication.java
@@ -0,0 +1,13 @@
+package com.example.elasticpocex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticPoCexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticPoCexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/elasticpocex/ElasticPoCexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elasticpocex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticPoCexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
